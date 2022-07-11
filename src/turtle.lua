8dd7a836dd64c03b3f9959ebef49945ef182c619
Jacob <jacob.heim@stud-mail.uni-wuerzburg.de>
2022-07-11 18:41:22 +0200

    restructured project

---
+++ src/turtle.lua
@@ -0,0 +1,9 @@
+turtle = {
+    forward = function ()end,
+    back = function ()end,
+    up = function ()end,
+    down = function ()end,
+    turnLeft = function ()end,
+    turnRight = function ()end ,
+    dig = function (side) return true, "reason" end,
+}
