afab929d9c2ae88c7c8dd4fcc8cc11a6263eaf81
Tank159 <l.schmalhorst@gmx.de>
2022-07-12 23:47:59 +0200

    kram

--- src/farming/farming.lua
+++ src/farming/farming.lua
@@ -3,7 +3,7 @@ slot = 2
 
 local function checkForItem(wantedAmt)
     local items = 0
-    for i=3, 15 do
+    for i=3, 16 do
         items = items+ turtle.getItemCount(i)
     end
     if items >= wantedAmt then
@@ -24,7 +24,7 @@ local function checkForFuel(wantedAmt)
         fuel = fuel+ turtle.getItemCount(i)
     end
 
-    if fuel >= wantedAmt then
+    if items >= wantedAmt then
         return true
         else
             return false
@@ -34,15 +34,31 @@ local function checkForFuel(wantedAmt)
 
 end
 
+local function refuel(amount)
+    i=0
+    turtle.select(1)
+    while turtle.getItemCount(1) > 0 and i<amount do
+        turtle.refuel()
+        i = i + 1
+    end
+    turtle.select(2)
+    while turtle.getItemCount(2) > 0 and i<amount do
+        turtle.refuel()
+        i = i + 1
+    end
+        
+end
+
+
 
 local function checkcurrentslot()
 
     if turtle.getItemCount(i) == 0 then
         slot=slot + 1
-        if slot==17 then
+        if slot > 16 then
             slot = 3
         end
-        
+
         turtle.select(slot)
         else
             return
@@ -54,7 +70,7 @@ local function plant(steps)
     for i=0, steps do
         
         checkcurrentslot()
-        turtle.placeDown()
+        turtle.placedown()
         turtle.forward()
     end
 end
@@ -90,24 +106,31 @@ local function replant()
 
     for field=0, 1 do
         for row=0, 6 do
-            plant(5)
-            if row % 2 == 0 then
+            plant(7)
+            if row % 2 then
                 turtle.turnRight()
                 turtle.forward()
-                
+                checkcurrentslot()
+                turtle.placedown()
+
                 turtle.turnRight()
+            else
+                turtle.turnLeft()
                 turtle.forward()
-                else
-                    turtle.turnLeft()
-                    turtle.forward()
-                    turtle.turnLeft()
+                checkcurrentslot()
+                turtle.placedown()
+
+                turtle.turnLeft()
             end
 
         end 
-        turtle.turnRight()
-        turtle.foreward()
-        turtle.turnRight()
-        plant(7)   
+        --turtle.turnRight()
+        --turtle.foreward()
+        --checkcurrentslot()
+        --turtle.placedown()
+
+        --turtle.turnRight()
+        --plant(7)   
 
 
     end
@@ -120,26 +143,27 @@ empty = 0
 
 while true do
 
-    if rs.getInput("bottom") and detected==0 then
+    if rs.getInput("Bottom") then
         detected = 1
-        print("detected")
-
+        print("watering")
     end
-        if detected==1 and not rs.getInput("bottom") then
-            detected = 0
-            print("planting")
-            empty = 0
-            empty = checkForItem(100)
-            empty = checkForFuel(10)
 
-            start_pos()
-            replant()
-            --if not empty then
-            --  replant()
-            --end
-      end      
+    if detected == 1 and not rs.getInput("Bottom") then
+            
+            detected = 0
+            empty = 0
+            empty = checkForItem(140)
+            empty = checkForFuel(3)
+
+            if not empty then
+              print("planting")
+              refuel(3)
+              replant()
+            end
         
-            print("wating")
+        else
+            print("waiting")
             sleep(2)
+    end
 
 end
